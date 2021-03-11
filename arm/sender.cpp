6d9b15209d118dba8119dc2b11bc1ab36f6e5e6b
Muchen He <mhe@ece.ubc.ca>
2021-03-10 23:06:28 -0800

    added f+r code

---
+++ arm/sender.cpp
@@ -0,0 +1,101 @@
+
+#include "util.hpp"
+
+/* Padding/dummy functions */
+void send_bit_0() { NOP_PADDING }
+void send_bit_1() { NOP_PADDING }
+void send_bit_2() { NOP_PADDING }
+void send_bit_3() { NOP_PADDING }
+void send_bit_4() { NOP_PADDING }
+void send_bit_5() { NOP_PADDING }
+void send_bit_6() { NOP_PADDING }
+void send_bit_7() { NOP_PADDING }
+void send_bit_8() { NOP_PADDING }
+
+/* Array to hold the covert channel bit addresses */
+ADDR_PTR sendbit_arr[BIT_WIDTH];
+
+/* Address to the sender/victim program */
+char *mapped;
+
+/**
+ * Send a specific bit of a 8-bit character
+ * @param: char c - the character
+ * @param: int i - the bit index
+ */
+inline void send_bit(char c, int i)
+{
+	/* If the bit is "1", then we perform a READ at the covert channel address */
+	if (i == 0 || c & (1 << (i - 1)))
+		char tmp = mapped[sendbit_arr[i]];
+}
+
+int main(void)
+{
+	printf("Victim/sender program now running.\n");
+	printf("Enter message to be sent through covert channel:\n");
+
+	/**
+	 * Setup the covert channel by obtaining the address of the victim
+	 * 1. Open the sender program
+	 * 2. Map it to memory to get its address
+	 */
+	int fd = open("sender", O_RDONLY);
+	mapped = (char *) mmap(NULL, 0x20000, PROT_READ, MAP_PRIVATE, fd, 0);
+
+	/**
+	 * For each bit of the transmission, we map it a unique address such that
+	 * it has its own cache line
+	 */
+	int mult = 15;
+	sendbit_arr[0] = ADDR_0 + mult * CACHE_LINE_SIZE;
+	sendbit_arr[1] = ADDR_1 + mult * CACHE_LINE_SIZE;
+	sendbit_arr[2] = ADDR_2 + mult * CACHE_LINE_SIZE;
+	sendbit_arr[3] = ADDR_3 + mult * CACHE_LINE_SIZE;
+	sendbit_arr[4] = ADDR_4 + mult * CACHE_LINE_SIZE;
+	sendbit_arr[5] = ADDR_5 + mult * CACHE_LINE_SIZE;
+	sendbit_arr[6] = ADDR_6 + mult * CACHE_LINE_SIZE;
+	sendbit_arr[7] = ADDR_7 + mult * CACHE_LINE_SIZE;
+	sendbit_arr[8] = ADDR_8 + mult * CACHE_LINE_SIZE;
+
+	bool sending = true;
+	while (sending)
+	{
+		char text_buf[128];
+		fgets(text_buf, sizeof(text_buf), stdin);
+		clock_t begin = clock();
+		for (int i = 0; i < 128; i++)
+		{
+			if (text_buf[i] == '\0')
+				break;
+
+			int c = text_buf[i];
+
+			/**
+			 * Pulse the bits to the covert channel
+			 */
+			CYCLES start_t = get_highres_time();
+			while (get_highres_time() - start_t < SLOT_HIT_CUTOFF * TIME_SLOT * SENDER_PULSE)
+			{
+				send_bit(c, 0);
+				send_bit(c, 1);
+				send_bit(c, 2);
+				send_bit(c, 3);
+				send_bit(c, 4);
+				send_bit(c, 5);
+				send_bit(c, 6);
+				send_bit(c, 7);
+				send_bit(c, 8);
+			}
+
+			wait_for_time(TIME_SLOT * 100);
+		}
+		clock_t end = clock();
+
+		printf("TOTAL TIME SPENT: %f\n", ((double)end - begin) / CLOCKS_PER_SEC);
+	}
+
+	printf("Sender finished.\n");
+
+	return 0;
+}
---
+++ arm/util.hpp
@@ -0,0 +1,130 @@
+#ifndef UTIL_H_
+#define UTIL_H_
+
+// You may only use fgets() to pull input from stdin
+// You may use any print function to stdout to print
+// out chat messages
+#include <stdio.h>
+
+// You may use memory allocators and helper functions
+// (e.g., rand()).  You may not use system().
+#include <stdlib.h>
+
+#include <inttypes.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+
+#include "padding.hpp"
+
+#define ADDR_PTR uint64_t
+#define CYCLES volatile uint64_t
+
+/* How many bytes are in a cache line */
+#define CACHE_LINE_SIZE 64
+
+/* Number of cycles considered as a threshold to be a hit */
+#define TIME_CUTOFF 200
+
+/* Time quantum for a single  */
+#define TIME_SLOT 2500
+
+
+#define SLOT_WINDOW_COUNT 10
+#define SLOT_HIT_CUTOFF 2
+#define SENDER_PULSE 4
+
+/* Channel bit width (how many bits to send concurrently) */
+#define BIT_WIDTH 9
+
+/* The addresses for each bit in the channel */
+#define ADDR_0 0x1176
+#define ADDR_1 0x22fd
+#define ADDR_2 0x3484
+#define ADDR_3 0x460b
+#define ADDR_4 0x5792
+#define ADDR_5 0x6919
+#define ADDR_6 0x7aa0
+#define ADDR_7 0x8c27
+#define ADDR_8 0x9dae
+
+/* Flush a single block given the virtual address addr */
+void flush_one_block(ADDR_PTR addr)
+{
+	asm volatile(
+		"clflush 0(%0)"
+		:
+		: "r"(addr));
+}
+
+CYCLES probe_one_block(ADDR_PTR adrs)
+{
+	CYCLES time;
+
+	asm __volatile__(
+		"  mfence             \n"
+		"  lfence             \n"
+		"  rdtsc              \n"
+		"  lfence             \n"
+		"  movl %%eax, %%esi  \n"
+		"  movl (%1), %%eax   \n"
+		"  lfence             \n"
+		"  rdtsc              \n"
+		"  subl %%esi, %%eax  \n"
+		"  clflush 0(%1)      \n"
+		: "=a"(time)
+		: "c"(adrs)
+		: "%esi", "%edx");
+	return time;
+}
+
+/* Measure the time it takes to access a block given a virtual address addr */
+CYCLES measure_one_block_access_time(ADDR_PTR addr)
+{
+	CYCLES cycles;
+
+	asm volatile(
+		"mov %1, %%r8\n\t"
+		"lfence\n\t"
+		"rdtsc\n\t"
+		"mov %%eax, %%edi\n\t"
+		"mov (%%r8), %%r8\n\t"
+		"lfence\n\t"
+		"rdtsc\n\t"
+		"sub %%edi, %%eax\n\t"
+		: "=a"(cycles) /*output*/
+		: "r"(addr)
+		: "r8", "edi");
+
+	return cycles;
+}
+
+CYCLES get_highres_time()
+{
+	volatile uint32_t time_lo;
+	volatile uint32_t time_hi;
+
+	/**
+	 * RDTSC returns the number of cycles since reset
+	 * Obtain high resolution cycle/timing information
+	 * 
+	 * ARMv7 has CCNT instruction
+	 */
+	asm volatile(
+		"rdtsc\n"
+		: "=a"(time_lo), "=d"(time_hi));
+
+	CYCLES time = (uint64_t)time_lo | (((uint64_t)time_hi) << 32);
+	return time;
+}
+
+void wait_for_time(CYCLES time)
+{
+	CYCLES start_t = get_highres_time();
+	while(get_highres_time() - start_t < time);
+}
+
+
+#endif
