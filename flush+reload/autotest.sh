0af35e21258c46a23de206cb891704865e0d62bb
Muchen He <i@muchen.ca>
2022-03-24 11:20:27 -0700

    cleanup f+r

--- flush+reload/autotest.sh
+++
@@ -1,4 +0,0 @@
-make clean && make CEXTRAS=-DRUN_ONCE
-printf '\n' | ./receiver 200 > receiver_out.txt &
-printf 'abcdefghijklmnopqrstuvwxyz0123456789_ABCDEFGHIJKLMNOPQRSTUVWXYZ_lcall#5\n' | ./sender
-cat receiver_out.txt
--- flush+reload/autotest_arm.sh
+++
@@ -1,3 +0,0 @@
-printf '\n' | ./receiver.arm 200 > receiver_out.txt &
-printf 'abcdefghijklmnopqrstuvwxyz0123456789_ABCDEFGHIJKLMNOPQRSTUVWXYZ_lcall#5\n' | ./sender.arm
-cat receiver_out.txt
